215367bc7696cf2c24b9a84300ab2fe7c614680e
adamaxy <adam996@libero.it>
2023-07-17 15:14:11 +0200

    upload es. 4

--- PrintYourNameAndSurname.java
+++
@@ -1,6 +0,0 @@
-public class PrintYourNameAndSurname {
-    public static void main(String[] args){
-    String name = "giovanni" , surName = "cassano";
-     System.out.println(name + " " + surName);
-    }
-}
---
+++ co/develhope/introduction/test.java
@@ -0,0 +1,15 @@
+package co.develhope.introduction;
+
+public class test {
+    public static void main(String [] arg){
+        int a = 15; a+= 5; a-= 4;
+        System.out.println(a);
+     int b = a; b+= 1;
+     if (b % 2 == 0 && b * (b+1) % 3 == 0 ){
+         System.out.println("il numero è pari ed è multiplo di 3");
+     }
+         else { System.out.println("Il numero non è pari e non è multiplo di 3");
+             System.out.println( "parliamo del numero  " + b);
+
+    }
+}}
