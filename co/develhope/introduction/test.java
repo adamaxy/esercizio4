eb0fa3f72cbecf80d05a51b044b41d05558c2e0b
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-07-21 11:20:47 +0200

    Update test.java
    correzione

--- co/develhope/introduction/test.java
+++ co/develhope/introduction/test.java
@@ -7,8 +7,9 @@ public class test {
         System.out.println(a);
      int b = a;
         b+= 1;
-    int x = b + 1;
-     if (b % 2 == 0 && b * x % 3 == 0 ){
+        System.out.println(b);
+    int x = b * (b + 1);
+     if (b % 2 == 0 && x % 3 == 0 ){
          System.out.println("il numero è pari ed è multiplo di 3");
      }
          else { System.out.println("Il numero non è pari e non è multiplo di 3");
